b22b85aa4ebf07dd179edffd447ddffed58452e5
ProkopenkoIA <boroda5384@mail.ru>
2023-03-02 01:30:08 +0500

    задача 60: трехмерная матрица

---
+++ Lesson1TaskDiagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача: матрицу 4*10 заполнить по диагонали
+
+void InputMatrix(int[,] matrix)
+{
+    int value , D;    
+
+    for (int i = 0; i < matrix.GetLongLength(0); i++)
+    {        
+        for (int j = 0; j < matrix.GetLongLength(1); j++)
+        {
+            D = i+j;
+            value = ((D*D+D)/2);     
+            matrix[i, j] = value;
+
+        }       
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLongLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLongLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");// \n - перенос строки
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Clear();
+
+Console.Write("Введите размер массива ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+int[,] matrix = new int[size[0], size[1]];
+
+InputMatrix(matrix);
+PrintMatrix(matrix);
+
+
---
+++ Lesson2Task60/Program.cs
@@ -0,0 +1,53 @@
+﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+void InputMatrixA(int[,,] matrix)
+{
+    Random n = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = n.Next(1, 101);
+            }
+        }
+    }
+}
+
+
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($"{matrix[i, j,z]} ({i}{j}{z}) \t");// \n - перенос строки
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
+
+
+
+Console.Clear();
+
+Console.Write("Введите размер матрицы А ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+int[,,] matrixA = new int[size[0], size[1], size[2]];
+
+InputMatrixA(matrixA);
+PrintMatrix(matrixA);
+
+
+
+
+
